b59185d10e2d369773ad5f6b517c57bff339ed4d
Jacqueline Kalmár <jaqika@gmail.com>
2024-05-28 13:40:11 +0000

    feat: add Google Font and modularize CSS in React
    Import DM Sans Google Font in the head of index.html
    
    Delete App.css and create App.module.css for global styles
    
    Apply DM Sans font and global styles in App.module.css
    
    Connect App.module.css to App.js and verify global styles
    
    Create NavBar.module.css for NavBar-specific styles
    
    Update NavBar.js to use styles from NavBar.module.css
    
    Implement responsive navbar toggler with hamburger to X transformation
    
    Remove old NavBar.css and replace with NavBar.module.css
    
    Add styles to NavBar.module.css for consistent UI
    
    BREAKING CHANGE: Old NavBar.css is deleted & replaced with NavBar.module.css

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,26 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.webp";
-import "./NavBar.css";
+import styles from "../styles/NavBar.module.css";
 
+
+/* responsive navbar toggler with hamburger to X transformation */
+/* Inspired by: "https://youtu.be/_MrShB9fh7U?si=na9LouEIB1-m1kMt# */
 const NavBar = () => {
+    const [expanded, setExpanded] = useState(false);
+
+    const handleToggle = () => {
+        setExpanded(!expanded);
+    };
+
     return (
-        <Navbar expand="md" fixed="top">
+        <Navbar className={styles.NavBar} expand="md" fixed="top" expanded={expanded}>
             <Container>
                 <Navbar.Brand>
                     <img src={logo} alt="logo" height="50" />
                 </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Toggle
+                    aria-controls="basic-navbar-nav"
+                    onClick={handleToggle}
+                    className={expanded ? `${styles['navbar-toggler']} ${styles['active']}` : styles['navbar-toggler']}
+                >
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </Navbar.Toggle>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto text-left">
                         <Nav.Link>
-                            <i className="fas fa-home icon-color"></i>Home
+                            <i className={`fas fa-home ${styles.iconColor}`}></i> Home
                         </Nav.Link>
                         <Nav.Link>
-                            <i className="fas fa-sign-in-alt"></i>Sign in
+                            <i className={`fas fa-sign-in-alt ${styles.iconColor}`}></i> Sign in
                         </Nav.Link>
                         <Nav.Link>
-                            <i className="fas fa-user-plus"></i>Sign up
+                            <i className={`fas fa-user-plus ${styles.iconColor}`}></i> Sign up
                         </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
