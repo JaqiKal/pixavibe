243e1a70a9a67183f63ab52d513ae29ac0fea234
Jacqueline Kalmár <jaqika@gmail.com>
2024-05-29 06:38:07 +0000

    refactor: Remove hamburger to X transformation

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,43 +1,27 @@
-import React, { useState } from "react";
+import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.webp";
 import styles from "../styles/NavBar.module.css";
 
 
-/* responsive navbar toggler with hamburger to X transformation */
-/* Inspired by: "https://youtu.be/_MrShB9fh7U?si=na9LouEIB1-m1kMt# */
 const NavBar = () => {
-    const [expanded, setExpanded] = useState(false);
-
-    const handleToggle = () => {
-        setExpanded(!expanded);
-    };
-
     return (
-        <Navbar className={styles.NavBar} expand="md" fixed="top" expanded={expanded}>
+        <Navbar className={styles.NavBar} expand="md" fixed="top">
             <Container>
                 <Navbar.Brand>
-                    <img src={logo} alt="logo" height="50" />
+                    <img src={logo} alt="logo" height="45" />
                 </Navbar.Brand>
-                <Navbar.Toggle
-                    aria-controls="basic-navbar-nav"
-                    onClick={handleToggle}
-                    className={expanded ? `${styles['navbar-toggler']} ${styles['active']}` : styles['navbar-toggler']}
-                >
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </Navbar.Toggle>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ml-auto text-left">
+                    <Nav className="ml-auto text-left"  >
                         <Nav.Link>
-                            <i className={`fas fa-home ${styles.iconColor}`}></i> Home
+                            <i className="fas fa-home"></i>Home
                         </Nav.Link>
                         <Nav.Link>
-                            <i className={`fas fa-sign-in-alt ${styles.iconColor}`}></i> Sign in
+                            <i className="fas fa-sign-in-alt"></i>Sign in
                         </Nav.Link>
                         <Nav.Link>
-                            <i className={`fas fa-user-plus ${styles.iconColor}`}></i> Sign up
+                            <i className="fas fa-user-plus"></i>Sign up
                         </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
