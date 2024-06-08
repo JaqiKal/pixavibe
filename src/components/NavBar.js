b82b7b90f37a3b5dd175da8b1f9601d58c2098be
Jacqueline Kalmár <jaqika@gmail.com>
2024-06-08 05:22:46 +0000

    feat: Add contact navlink in navbar for logged in user

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -55,6 +55,13 @@ const NavBar = () => {
       >
         <i className="fas fa-heart"></i>Liked
       </NavLink>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/contact"
+      >
+        <i className="fas fa-envelope"></i>Contact
+      </NavLink>
       <NavLink className={styles.NavLink} to="/" onClick={handleSignOut}>
         <i className="fas fa-sign-out-alt"></i>Sign out
       </NavLink>
