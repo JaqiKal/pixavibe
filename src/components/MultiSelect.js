b6cb8f9d3f3438589232631e005edfc42bed2757
Jacqueline Kalmár <jaqika@gmail.com>
2024-06-14 09:50:39 +0000

    chores: Comment code in Multiselect.js

--- src/components/MultiSelect.js
+++ src/components/MultiSelect.js
@@ -1,16 +1,28 @@
+/**
+ * Using the React Multiselect Dropdown component to cater for handling
+ * hashtag selection . Fetches hashtags from the API and allows
+ * users to select or deselect them.
+ */
 import { useState, useEffect } from "react";
 import Multiselect from "multiselect-react-dropdown";
 import { fetchHashtags } from "../api/fetchHashtags";
 
+/**
+ * Props:
+ * - selectedHashtags (Array): The current list of selected hashtags.
+ * - setSelectedHashtags (Function): Function to update the selected hashtags state.
+ */
 const MultiSelect = ({ selectedHashtags, setSelectedHashtags }) => {
   const [hashtags, setHashtags] = useState([]);
 
   useEffect(() => {
+    // Fetch hashtags from the API on component mount.
     const getHashtags = async () => {
       try {
         const hashtags = await fetchHashtags();
         setHashtags(hashtags);
       } catch (error) {
+        // In case of an error, set an empty array for hashtags.
         setHashtags([]);
       }
     };
@@ -18,10 +30,12 @@ const MultiSelect = ({ selectedHashtags, setSelectedHashtags }) => {
     getHashtags();
   }, []);
 
+  // Handle the selection of a new hashtag.
   const onSelect = (selectedList, selectedItem) => {
     setSelectedHashtags([...selectedList]);
   };
 
+  // Handle the removal of a selected hashtag.
   const onRemove = (selectedList, removedItem) => {
     setSelectedHashtags([...selectedList]);
   };
